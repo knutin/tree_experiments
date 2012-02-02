b3c223525be145809fd9af7bf982569ca6ef07ae
Knut Nesheim <knutin@gmail.com>
2012-02-02 18:56:39 +0100

    Improved trie tuple implemantion. Includes simple quickcheck test.

--- src/trie_tuple.erl
+++ src/trie_tuple.erl
@@ -1,14 +1,18 @@
 -module(trie_tuple).
 -compile([export_all]).
 
+-include_lib("eqc/include/eqc.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-new() ->
-    erlang:make_tuple(10, nil).
 
+new() ->
+    {trie, new_children()}.
+
+insert({trie, Children}, Keys, V) ->
+    {trie, insert(Children, Keys, V)};
 
 insert(nil, [K], V) ->
-    setelement(K+1, new(), {V, nil});
+    setelement(K+1, new_children(), {V, nil});
 
 insert(Nodes, [K], V) ->
     case element(K+1, Nodes) of
@@ -19,52 +23,101 @@ insert(Nodes, [K], V) ->
     end;
 
 insert(nil, Keys, V) ->
-    insert(new(), Keys, V);
+    insert(new_children(), Keys, V);
 
 insert(Nodes, [K | Rest], V) ->
-    setelement(K+1, Nodes, insert(element(K+1, Nodes), Rest, V)).
+    case element(K+1, Nodes) of
+        {Val, Children} ->
+            setelement(K+1, Nodes, {Val, insert(Children, Rest, V)});
+        nil ->
+            setelement(K+1, Nodes, {nil, insert(new_children(), Rest, V)})
+    end.
+
+new_children() ->
+    erlang:make_tuple(10, nil).
 
 
+find({_, Children}, Keys) ->
+    find(Children, Keys);
 
+find(Children, [K]) ->
+    element(1, element(K+1, Children));
+
+find(Children, [K | Rest]) when size(Children) =:= 10 ->
+    find(element(K+1, Children), Rest);
 
 find(nil, _) ->
-    not_found;
-find(Nodes, [K]) ->
-    element(1, element(K+1, Nodes));
-find(Nodes, [K | Rest]) ->
-    find(element(K+1, Nodes), Rest).
+    not_found.
 
+
+
+
+
+%% find(_, []) ->
+%%     not_found;
+
+
+
+%% find({_, Children}, [K]) ->
+%%     element(1, element(K+1, Children));
+%% %% find(Nodes, [K]) ->
+%% %%     error_logger:info_msg("Nodes: ~p, K: ~p~n", [Nodes, K+1]),
+%% %%     element(1, element(K+1, Nodes));
+
+%% find({_, Children}, [K | Rest]) ->
+%%     io:format("recursing, k: ~p, rest: ~p~n", [K, Rest]),
+%%     find(element(K+1, Children), Rest).
+%% %% find(Nodes, [K | Rest]) when size(Nodes) =:= 10 ->
+%% %%     find(element(K+1, Nodes), Rest).
 
 
 
 int2key(I) ->
-    lists:map(fun (D) ->
-                      element(1, string:to_integer([D]))
-              end, integer_to_list(I)).
+    list2key(integer_to_list(I)).
 
+list2key(L) ->
+    lists:map(fun ($0) -> 0;
+                  ($1) -> 1;
+                  ($2) -> 2;
+                  ($3) -> 3;
+                  ($4) -> 4;
+                  ($5) -> 5;
+                  ($6) -> 6;
+                  ($7) -> 7;
+                  ($8) -> 8;
+                  ($9) -> 9;
+                  (_) -> throw(badarg)
+              end, L).
 
 %%
 %% TESTS
 %%
 
+
+simple_test() ->
+    T0 = insert(new(), [1], key1),
+    ?assertEqual(key1, find(T0, [1])).
+
 insert_test() ->
-    T0 = insert(new(), [1, 1, 1], <<255>>),
-    T1 = insert(T0, [1, 1, 1, 1], <<254>>),
-    ?assertEqual(<<255>>, find(T1, [1, 1, 1])),
-    ?assertEqual(<<254>>, find(T1, [1, 1, 1, 1])).
+    T0 = insert(new(), [1, 1, 1], key1),
+    T1 = insert(T0, [1, 1, 1, 1], key2),
+    T2 = insert(T1, [1], key3),
+    ?assertEqual(key1, find(T2, [1, 1, 1])),
+    ?assertEqual(key2, find(T2, [1, 1, 1, 1])),
+    ?assertEqual(key3, find(T2, [1])).
 
 
-big_insert_test() ->
-    Start = 100000000000000,
-    N = 100000,
-    Keys = lists:map(fun (I) -> int2key(I) end,
-                     lists:seq(Start, Start+N)),
+%% big_insert_test() ->
+%%     Start = 100000000000000,
+%%     N = 100000,
+%%     Keys = lists:map(fun (I) -> int2key(I) end,
+%%                      lists:seq(Start, Start+N)),
 
-    Tree = lists:foldl(fun (K, T) ->
-                               insert(T, K, <<255>>)
-                       end, new(), Keys),
-    error_logger:info_msg("~p keys in ~p mb~n",
-                          [N, (erts_debug:flat_size(Tree) * 8) / 1024 / 1024]).
+%%     Tree = lists:foldl(fun (K, T) ->
+%%                                insert(T, K, <<255>>)
+%%                        end, new(), Keys),
+%%     error_logger:info_msg("~p keys in ~p mb~n",
+%%                           [N, (erts_debug:flat_size(Tree) * 8) / 1024 / 1024]).
 
 
 get_bench() ->
@@ -92,3 +145,27 @@ multi_search(T, Keys) ->
     lists:map(fun (K) ->
                       find(T, K)
               end, Keys).
+
+
+prop_test() ->
+    ?assert(quickcheck(prop_insert_0())),
+    ?assert(quickcheck(prop_insert_1())).
+
+keys() ->
+    ?SIZED(Size, list(choose(0, Size))).
+
+prop_insert_0() ->
+    ?FORALL({K, Xs}, {choose(0, 100000000), keys()},
+            begin
+                %%io:format("insert key: ~w, keys: ~w~n", [K, Xs]),
+                Tree = lists:foldl(fun (I, T) -> insert(T, int2key(I), I) end,
+                                   new(), Xs),
+                %%io:format("tree: ~p~n", [Tree]),
+                K =:= find(insert(Tree, int2key(K), K), int2key(K))
+            end).
+
+prop_insert_1() ->
+    ?FORALL(K, choose(1, 100000000),
+            begin
+                K =:= find(insert(new(), int2key(K), K), int2key(K))
+            end).
